70ed3e9fa176a3294bfe0b50d9a16a93e820d07a
Ed Tansey <office.fwd@yahoo.com>
2021-12-25 13:51:30 +0000

    Add app elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { FC } from 'react'
 import './App.css'
 
-function App() {
-  return <div className='App'></div>
+const App: FC = () => {
+  return (
+    <div className='App'>
+      {' '}
+      <div className='header'>
+        <div className='inputContainer'>
+          <input type='text' placeholder='Task...' />
+          <input type='number' placeholder='Dead (in Days).' />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className='todoList'></div>
+    </div>
+  )
 }
 
 export default App
