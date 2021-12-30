e944b7e0bfe2655b50f17001155a864e4b472f82
Ed Tansey <office.fwd@yahoo.com>
2021-12-30 15:48:30 +0000

    Add deletefunctionality to button

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,14 @@ const App: FC = () => {
     setDeadline(0)
   }
 
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName != taskNameToDelete
+      })
+    )
+  }
+
   return (
     <div className='App'>
       {' '}
@@ -49,7 +57,7 @@ const App: FC = () => {
       </div>
       <div className='todoList'>
         {todoList.map((task: ITask, key: number) => {
-          return <TodoTask key={key} task={task} />
+          return <TodoTask key={key} task={task} completeTask={completeTask} />
         })}
       </div>
     </div>
